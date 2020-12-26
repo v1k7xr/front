9ee8fa465ae5a0dc86848b7daedd5f366e8cde24
Viktor <eugenenickolsky@yandex.ru>
2020-12-26 09:01:56 +0300

    added simple logic

--- src/components/UserDataActivInput.vue
+++ src/components/UserDataActivInput.vue
@@ -1,9 +1,32 @@
 <template>
     <div>
-        <p> {{ helloString }} </p>
         <input v-model="cityName" placeholder="Название города">
+        <br>
         <input v-model="hoursNumber" placeholder="Колличество часов">
+        <br>
         <input v-model="passengeNumber" placeholder="Колличество пассажиров">
+        <br>
+
+        <input type="checkbox" id="onplace" value="onplace" v-model="activList">
+            <label for="onplace">Не выходя из аэропорта</label> <br>
+
+        <input type="checkbox" id="luggage" value="luggage" v-model="activList">
+            <label for="luggage">Я с багажом</label> <br>
+
+        <input type="checkbox" id="hotel" value="hotel" v-model="activList">
+            <label for="hotel">Отели</label> <br>
+
+        <input type="checkbox" id="entertainment" value="entertainment" v-model="activList">
+            <label for="entertainment">Развлечения</label> 
+        <input type="checkbox" id="entertainment_kidfriendly" value="entertainment_kidfriendly" v-model="activList">
+            <label for="entertainment_kidfriendly">Можно с детьми</label> <br>
+            <br>
+
+        <input type="checkbox" id="eat" value="eat" v-model="activList">
+            <label for="eat">Покушать</label> <br>
+
+        <input type="checkbox" id="interesting_places" value="interestingPlaces" v-model="activList">
+            <label for="interesting_places">Интересные места</label> <br>
 
         <button v-on:click="testApi">Тест API</button>
     </div>
@@ -19,20 +42,28 @@ export default {
             helloString : null,
             cityName : '',
             hoursNumber: 0,
-            passengeNumber : 0,
-            activList : {
-
-            }
+            passengeNumber : 0,  
+            activList : []
         }
     },
 
     methods: {
         testApi() {
+            let jsonData = {
+                cityName: this.cityName,
+                hoursNumber: this.hoursNumber,
+                passengeNumber : this.passengeNumber,
+                activList : this.activList,
+            }
+
+            console.log(jsonData);
+
             axios
-            .get(httpHeader)
-            .then(response => (this.helloString = response))
-            console.log(httpHeader);
-        }
+              .post(httpHeader, jsonData)
+              .then(response => {
+                  console.log(response);
+              })
+        }, 
     }
 }
 </script>
